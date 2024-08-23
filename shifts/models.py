cd7da9ddfb02f9ad1498a8d71f11743d7884421e
benitha200 <benithaiyuyisenga2002@gmail.com>
2024-08-23 10:28:19 +0200

    add shift data

--- shifts/models.py
+++ shifts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from accounts.models import CustomUser
 
 # Create your models here.
 
@@ -6,12 +7,18 @@ class Shift(models.Model):
     shift_no=models.IntegerField()
     activity=models.CharField(max_length=255)
     date=models.DateField()
+    supplier=models.CharField(max_length=255)
+    shift_type=models.CharField(max_length=255)
+    coffee_type=models.CharField(max_length=255)
+    output_batchno=models.IntegerField()
+    location_of_batch=models.CharField(max_length=255)
+    created_by=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now=True)
 
 
 class ShiftDetails(models.Model):
     shift=models.ForeignKey(Shift,on_delete=models.CASCADE)
-    supplier=models.CharField(max_length=255)
+
     grade=models.CharField(max_length=50)
     total_kgs=models.IntegerField()
     total_bags=models.IntegerField()
