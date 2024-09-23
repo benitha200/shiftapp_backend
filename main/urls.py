cb8f5aecf7bc9301bb212b4ea5345ef708b76fc5
benitha200 <benithaiyuyisenga2002@gmail.com>
2024-09-23 14:42:20 +0200

    changes

--- main/urls.py
+++ main/urls.py
@@ -48,6 +48,6 @@ urlpatterns = [
     path('api/', include('api.urls')),
     
     # Swagger URLs
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
